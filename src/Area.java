39377b9425f02bf084a1e976572204bd6670d0e1
ThePhaseCat <alexmgamer10@gmail.com>
2023-02-07 12:23:12 -0700

    help

--- src/Area.java
+++ src/Area.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 public class Area{
 
     static int revertState = 0;
+    static String input = "";
 
     public static int calArea(int len, int wid, int hei){
         int sum = len * wid * hei;
@@ -20,16 +21,14 @@ public class Area{
         return revertState;
     }
 
-    public static void AreaMenu(){
-        ArrayList<Integer> areaData = new ArrayList<Integer>();
-        areaData.add(0);
-        areaData.add(0);
-        areaData.add(0);
-        areaData.add(0);
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
+    public static void AreaMenu() {
 
-        //System.out.println("Welcome to the MineCalc Area Service!");
+        ArrayList<Integer> areaData = new ArrayList<Integer>(5);
+        Scanner keyboard = new Scanner(System.in);
+        //String input = "";
+        while (!input.equals("Q")) {
+
+        System.out.println("Welcome to the MineCalc Area Service!");
         System.out.println("1. Change Area Data");
         System.out.println("2. Get Area Data");
         System.out.println("3. Get Estimations from Area Data");
@@ -38,33 +37,34 @@ public class Area{
         input = keyboard.nextLine();
         input = input.toUpperCase();
 
-        if (input.equals("1")){
-            System.out.print("Length of Area: ");
-            areaData.set(0, keyboard.nextInt());
-            System.out.print("Width of Area: ");
-            areaData.set(1, keyboard.nextInt());
-            System.out.print("Height of Area: ");
-            areaData.set(2, keyboard.nextInt());
-            areaData.set(3, calArea(areaData.get(0), areaData.get(1), areaData.get(2)));
-            AreaMenu();
-        }
-        if (input.equals("2")) {
-            System.out.println("Length of Area: " + areaData.get(0));
-            System.out.println("Width of Area: " + areaData.get(1));
-            System.out.println("Height of Area: " + areaData.get(2));
-            System.out.println("Area: " + areaData.get(3));
-        }
-        if (input.equals("3")){
-            AreaEstimation area = new AreaEstimation();
-            while(area.revertState == 0){
-                area.EstimateArea(areaData.get(3));
-                //find what 2*2 is
-                //find what 3*3 is
+            if (input.equals("1")) {
+                System.out.print("Length of Area: ");
+                areaData.set(0, keyboard.nextInt());
+                System.out.print("Width of Area: ");
+                areaData.set(1, keyboard.nextInt());
+                System.out.print("Height of Area: ");
+                areaData.set(2, keyboard.nextInt());
+                areaData.set(3, calArea(areaData.get(0), areaData.get(1), areaData.get(2)));
+                AreaMenu();
             }
+            if (input.equals("2")) {
+                System.out.println("Length of Area: " + areaData.get(0));
+                System.out.println("Width of Area: " + areaData.get(1));
+                System.out.println("Height of Area: " + areaData.get(2));
+                System.out.println("Area: " + areaData.get(3));
+            }
+            if (input.equals("3")) {
+                AreaEstimation area = new AreaEstimation();
+                while (area.revertState == 0) {
+                    area.EstimateArea(areaData.get(3));
+                    //find what 2*2 is
+                    //find what 3*3 is
+                }
+            }
+            //if (input.equals("Q")) {
+               // revertState = 1;
+            //}
         }
-        if (input.equals("Q")){
-            revertState = 1;
-    }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner; //for reading keyboard input and Files
 import java.io.BufferedReader; //Fast way to read large files and other data streams
 import java.io.FileReader; //used when using BufferedReader to read a File
@@ -9,28 +11,106 @@ import java.util.ArrayList;
 
 public class Main{
 
+    static int length = 0;
+    int width = 0;
+    int height = 0;
+    int area = 0;
+
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
+        JFrame mainScreen = new JFrame("MineCalc - Selection Service");
+        mainScreen.setLayout(new BoxLayout(mainScreen.getContentPane(), BoxLayout.Y_AXIS));
 
-        while (!input.equals("Q")) {
-            System.out.println("Welcome to MineCalc!");
-            System.out.println("What service would you like to use?");
-            System.out.println("1. Area Service");
-            System.out.println("2. Structure Service");
-            System.out.println("3. Block/Item Index Service");
+        JButton areaButton = new JButton("Area Service");
+        JButton structureButton = new JButton("Structure Service");
+        JButton blockButton = new JButton("Block/Item Index Service");
+        areaButton.setBounds(0, 0, 100, 100);
+        structureButton.setBounds(0, 0, 100, 100);
+        blockButton.setBounds(0, 0, 100, 100);
 
-            input = keyboard.nextLine();
-            input = input.toUpperCase();
+        areaButton.addActionListener(e -> {
+            area();
+        });
 
-            if (input.equals("1")){
-                Area AreaMenu = new Area();
-                while(AreaMenu.revertState == 0){
-                    AreaMenu.AreaMenu();
-                }
-            }
-        }
+        mainScreen.add(areaButton);
+        mainScreen.add(structureButton);
+        mainScreen.add(blockButton);
+
+        mainScreen.setSize(300, 200);
+        mainScreen.setVisible(true);
+        mainScreen.setLocationRelativeTo(null);
+        mainScreen.setLocation(0, 0);
+        mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 
+    public static void area(){
+
+        JFrame areaScreen = new JFrame("MineCalc - Area Service");
+        areaScreen.setLayout(new BoxLayout(areaScreen.getContentPane(), BoxLayout.Y_AXIS));
+
+        JButton changeDataButton = new JButton("Change Area Data");
+        JButton getDataButton = new JButton("Get Area Data");
+        JButton getEstimationsButton = new JButton("Get Estimations from Area Data");
+
+        changeDataButton.setBounds(0, 0, 100, 100);
+        getDataButton.setBounds(0, 0, 100, 100);
+        getEstimationsButton.setBounds(0, 0, 100, 100);
+
+        changeDataButton.addActionListener(e -> {
+            areaChangeData();
+        });
+
+        getDataButton.addActionListener(e -> {
+            areaGetData();
+        });
+
+        getEstimationsButton.addActionListener(e -> {
+            areaGetEstimations();
+        });
+
+        areaScreen.add(changeDataButton);
+        areaScreen.add(getDataButton);
+        areaScreen.add(getEstimationsButton);
+
+        areaScreen.setSize(300, 200);
+        areaScreen.setVisible(true);
+        areaScreen.setLocationRelativeTo(null);
+        areaScreen.setLocation(200, 200);
+
+
+    }
+
+    public static void areaChangeData(){
+
+        JFrame areaChangeDataScreen = new JFrame("MineCalc - Area Service - Change Area Data");
+        areaChangeDataScreen.setLayout(new BoxLayout(areaChangeDataScreen.getContentPane(), BoxLayout.Y_AXIS));
+
+
+        JTextField lengthField = new JTextField();
+        JTextField widthField = new JTextField();
+        JTextField heightField = new JTextField();
+
+        lengthField.addActionListener(e -> {
+            length = Integer.parse()lengthField.getText();
+        });
+
+        areaChangeDataScreen.add(lengthField);
+        areaChangeDataScreen.add(widthField);
+        areaChangeDataScreen.add(heightField);
+
+        areaChangeDataScreen.setSize(300, 200);
+        areaChangeDataScreen.setVisible(true);
+        areaChangeDataScreen.setLocationRelativeTo(null);
+        areaChangeDataScreen.setLocation(400, 200);
+    }
+
+    public static void areaGetData(){
+
+    }
+
+    public static void areaGetEstimations(){
+
+    }
 
 }
