51282502c27b1661db5120d16a0ee9d13069f7d9
ThePhaseCat <alexmgamer10@gmail.com>
2023-02-09 11:16:45 -0700

    more stuff!
    -Finished Change Area Data Screen
    -Created and Finished Get Area Data Screen
    -Experimented with the Area Estimations Screen (To be done next, and will require more methods)
    -Added new calArea method for calculating area (Need to make it set the area variable next)

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,9 @@ import java.util.ArrayList;
 public class Main{
 
     static int length = 0;
-    int width = 0;
-    int height = 0;
-    int area = 0;
+    static int width = 0;
+    static int height = 0;
+    static int area = 0;
 
     public static void main(String[] args) {
         JFrame mainScreen = new JFrame("MineCalc - Selection Service");
@@ -86,31 +86,88 @@ public class Main{
         JFrame areaChangeDataScreen = new JFrame("MineCalc - Area Service - Change Area Data");
         areaChangeDataScreen.setLayout(new BoxLayout(areaChangeDataScreen.getContentPane(), BoxLayout.Y_AXIS));
 
+        JLabel areaChangeDataLabel = new JLabel("Enter information here (Press enter when done with input to save selection)");
+        areaChangeDataLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel lengthLabel = new JLabel("Length: " + length);
+        JLabel widthLabel = new JLabel("Width: " + width);
+        JLabel heightLabel = new JLabel("Height: "+ height);
+
+        lengthLabel.setHorizontalAlignment(JLabel.CENTER);
+        widthLabel.setHorizontalAlignment(JLabel.CENTER);
+        heightLabel.setHorizontalAlignment(JLabel.CENTER);
 
         JTextField lengthField = new JTextField();
         JTextField widthField = new JTextField();
         JTextField heightField = new JTextField();
 
+        lengthField.setHorizontalAlignment(JTextField.CENTER);
+        widthField.setHorizontalAlignment(JTextField.CENTER);
+        heightField.setHorizontalAlignment(JTextField.CENTER);
+
         lengthField.addActionListener(e -> {
-            length = Integer.parse()lengthField.getText();
+            length = Integer.parseInt(lengthField.getText());
+            lengthLabel.setText("Length: " + length);
         });
 
+        widthField.addActionListener(e -> {
+            width = Integer.parseInt(widthField.getText());
+            widthLabel.setText("Width: " + width);
+        });
+
+        heightField.addActionListener(e -> {
+            height = Integer.parseInt(heightField.getText());
+            heightLabel.setText("Height: " + height);
+            //areaGetData();
+            //areaChangeDataScreen.dispose();
+        });
+
+        areaChangeDataScreen.add(areaChangeDataLabel);
+        areaChangeDataScreen.add(lengthLabel);
         areaChangeDataScreen.add(lengthField);
+        areaChangeDataScreen.add(widthLabel);
         areaChangeDataScreen.add(widthField);
+        areaChangeDataScreen.add(heightLabel);
         areaChangeDataScreen.add(heightField);
 
-        areaChangeDataScreen.setSize(300, 200);
+
+        areaChangeDataScreen.setSize(500, 300);
         areaChangeDataScreen.setVisible(true);
         areaChangeDataScreen.setLocationRelativeTo(null);
         areaChangeDataScreen.setLocation(400, 200);
     }
 
     public static void areaGetData(){
+        JFrame areaGetDataScreen = new JFrame("MineCalc - Area Service - Get Area Data");
+        areaGetDataScreen.setLayout(new BoxLayout(areaGetDataScreen.getContentPane(), BoxLayout.Y_AXIS));
 
+        JLabel lengthLabel = new JLabel("Length: " + length);
+        JLabel widthLabel = new JLabel("Width: " + width);
+        JLabel heightLabel = new JLabel("Height: " + height);
+        JLabel areaLabel = new JLabel("Area: " + calArea(length, width, height));
+
+        lengthLabel.setHorizontalAlignment(JLabel.CENTER);
+        widthLabel.setHorizontalAlignment(JLabel.CENTER);
+        heightLabel.setHorizontalAlignment(JLabel.CENTER);
+        areaLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        areaGetDataScreen.add(lengthLabel);
+        areaGetDataScreen.add(widthLabel);
+        areaGetDataScreen.add(heightLabel);
+        areaGetDataScreen.add(areaLabel);
+
+        areaGetDataScreen.setSize(300, 200);
+        areaGetDataScreen.setVisible(true);
+        areaGetDataScreen.setLocationRelativeTo(null);
+        areaGetDataScreen.setLocation(600, 200);
     }
 
     public static void areaGetEstimations(){
 
     }
 
+    public static int calArea(int len, int wid, int hei){
+        int sum = len * wid * hei;
+        return sum;
+    }
 }
